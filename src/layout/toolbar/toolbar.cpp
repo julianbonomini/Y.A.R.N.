e9884aea4c158e13fcc54cead0e8483ed1629886
Julian Bonomini <julian.bonomini@gmail.com>
2025-04-10 22:55:12 +0200

    toolbar snap to grid

--- src/layout/toolbar/toolbar.cpp
+++ src/layout/toolbar/toolbar.cpp
@@ -8,29 +8,35 @@ Toolbar::Toolbar(sf::RenderWindow &window, const sf::Font &font)
     : window(window), font(font) {
 }
 
-
 void Toolbar::draw() {
+    // Position for OS name (top-left)
+    float textX = Layout::PADDING;
+    float textY = Layout::PADDING;
+
     sf::Text osName(font, "NOOP");
     osName.setCharacterSize(FontSizes::TOOLBAR);
     osName.setFillColor(Colors::BLACK);
-    osName.setPosition({10.f, 10.f});
+    sf::Vector2f osNamePos = UIHelpers::snapToGrid({textX, textY});
+    osName.setPosition(osNamePos);
     window.draw(osName);
 
-    // Get current date and time
+    // Date/time (top-right)
     time_t now = std::time(nullptr);
     char timeStr[64];
     std::strftime(timeStr, sizeof(timeStr), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
+
     sf::Text dateAndTime(font, timeStr);
     dateAndTime.setCharacterSize(FontSizes::TOOLBAR);
     dateAndTime.setFillColor(Colors::BLACK);
-    sf::Vector2f dateAndTimePosition = UIHelpers::snapToGrid({window.getSize().x - 145.f, 10.f});
-    dateAndTime.setPosition(dateAndTimePosition);
+
+    float rightX = DisplayConfig::SCREEN_WIDTH - dateAndTime.getLocalBounds().size.x - Layout::PADDING;
+    sf::Vector2f dateTimePos = UIHelpers::snapToGrid({rightX, textY});
+    dateAndTime.setPosition(dateTimePos);
     window.draw(dateAndTime);
 
-    // Draw a thin horizontal line below the text
-    sf::RectangleShape line(sf::Vector2f(window.getSize().x, LineStyles::LINE_THICKNESS));
+    // Line separator below toolbar
+    sf::RectangleShape line(sf::Vector2f(DisplayConfig::SCREEN_WIDTH, LineStyles::LINE_THICKNESS));
     line.setFillColor(Colors::GRAY);
     line.setPosition({0.f, Layout::TOOLBAR_HEIGHT});
     window.draw(line);
-
 }
