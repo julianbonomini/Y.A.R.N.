eacb039d1f544df8821886abfe2c32d09037b0c1
Julian Bonomini <julian.bonomini@gmail.com>
2025-04-13 01:00:17 +0200

    make clean

--- scripts/package-linux.sh
+++ scripts/package-linux.sh
@@ -85,6 +85,7 @@ install_dependencies() {
 # Function to build the application using Makefile
 build_application() {
   echo "Building the application using Makefile..."
+  make clean
   make linux
 }
 
