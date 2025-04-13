cbee0600004233c8cf4f55ae90c80240450fa22c
Julian Bonomini <julian.bonomini@gmail.com>
2025-04-13 11:48:35 +0200

    add icon

--- scripts/ubuntu/package.sh
+++ scripts/ubuntu/package.sh
@@ -57,7 +57,7 @@ prepare_appimage_structure() {
       echo "Warning: Desktop entry ($DESKTOP_FILE) not found."
   fi
   if [ -f "$ICON_FILE" ]; then
-      cp "$ICON_FILE" "$APP_DIR/"
+      cp "$APP_DIR/usr/bin/assets/images/icon.png" "$APP_DIR/usr/share/icons/hicolor/256x256/apps/YARN.png"
   else
       echo "Warning: Icon file ($ICON_FILE) not found."
   fi
