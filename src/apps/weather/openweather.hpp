88752b507f936232975bc624fbebe8436fe60ead
Julian Bonomini <julian.bonomini@gmail.com>
2025-04-14 22:45:36 +0200

    fix optional type issue

--- src/apps/weather/openweather.hpp
+++ src/apps/weather/openweather.hpp
@@ -3,10 +3,34 @@
 
 #include <nlohmann/json.hpp>
 
+#include "../../common/logger.hpp"
+#include "../../core/http/http.hpp"
+
 
 class OpenWeather {
 public:
-    static std::optional<nlohmann::json> getWeather();
+    static nlohmann::json getWeather() {
+        std::string api_key = "e8409838ac1968c4c525799b5ac34da0"; // Replace with your OpenWeatherMap API key
+        std::string city = "Amsterdam";
+        std::map<std::string, std::string> query_params = {
+            {"q", city},
+            {"appid", api_key}
+        };
+        std::map<std::string, std::string> headers = {};
+        std::string host = "api.openweathermap.org";
+        std::string path = "/data/2.5/weather";
+        std::string scheme = "https";
+
+        auto response = Http::GET(host, path, query_params, headers, scheme);
+        if (response) {
+            auto data = response.value();
+            return data;
+        } else {
+            Logger::error("Error: Could not get weather data!");
+            nlohmann::json emptyData = {};
+            return emptyData;
+        }
+    };
 };
 
 
